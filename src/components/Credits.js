e7c5c51d2bef339cec0f163bfe56d6746abaaad8
Jacky Chen <jackyc4200@gmail.com>
2023-10-30 22:59:22 -0400

    credit and debit list

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -4,16 +4,32 @@ src/components/Credits.js
 The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Credits = (props) => {
+  let creditsView = () => {
+    const { credits } = props;
+    return credits.map((credit) => {
+      // Extract "id", "amount", "description" and "date" properties of each debits JSON array element
+      let date = credit.date.slice(0, 10);
+      return (
+        <li key={credit.id}>
+          {credit.amount} {credit.description} {date}
+        </li>
+      );
+    });
+  };
+
   return (
     <div>
       <h1>Credits</h1>
-      <br/>
+
+      {creditsView()}
+
+      <br />
       <Link to="/">Return to Home</Link>
     </div>
   );
-}
+};
 
 export default Credits;
--- src/components/Debits.js
+++ src/components/Debits.js
@@ -4,17 +4,22 @@ src/components/Debits.js
 The Debits component contains information for Debits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Debits = (props) => {
   // Create the list of Debit items
   let debitsView = () => {
     const { debits } = props;
-    return debits.map((debit) => {  // Extract "id", "amount", "description" and "date" properties of each debits JSON array element
-      let date = debit.date.slice(0,10);
-      return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
+    return debits.map((debit) => {
+      // Extract "id", "amount", "description" and "date" properties of each debits JSON array element
+      let date = debit.date.slice(0, 10);
+      return (
+        <li key={debit.id}>
+          {debit.amount} {debit.description} {date}
+        </li>
+      );
     });
-  }
+  };
   // Render the list of Debit items and a form to input new Debit item
   return (
     <div>
@@ -27,10 +32,10 @@ const Debits = (props) => {
         <input type="number" name="amount" />
         <button type="submit">Add Debit</button>
       </form>
-      <br/>
+      <br />
       <Link to="/">Return to Home</Link>
     </div>
   );
-}
+};
 
 export default Debits;
